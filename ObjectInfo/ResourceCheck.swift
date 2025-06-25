423a81ad39ce36f9a450612fdc12db9fde45454a
leslie helou <leslie@ccs-indy.com>
2025-06-24 23:37:49 -0400

    add version check fix issue looking up content filter payload in configuration profiles

---
+++ ObjectInfo/ResourceCheck.swift
@@ -0,0 +1,214 @@
+//
+//  Copyright 2025, Jamf
+//
+
+import Foundation
+//import OSLog
+
+class ResourceCheck: NSObject, URLSessionDelegate {
+    
+    static let shared = ResourceCheck()
+    
+    @MainActor func launchCheck(prohibitedCheck: Bool = true, versionCheck: Bool = true) async {
+        WriteToLog.shared.message("[launchCheck] performing launch check")
+        let prohibitedVersions = prohibitedCheck ? await ResourceCheck.shared.prohibited(): []
+        let prohibited = prohibitedVersions.contains(AppInfo.version)
+        
+        let versionResult = await ResourceCheck.shared.version(forceCheck: versionCheck)
+//        let versionResult = await ResourceCheck.shared.version(forceCheck: prohibited)
+        
+        if prohibited {
+            _ = Alert.shared.display(header: "", message: "This version, v\(AppInfo.version), is prohibited from running.", secondButton: "")
+            WriteToLog.shared.message("[launchCheck] this version, v\(AppInfo.version), is prohibited from running. Application terminated.")
+            if !versionResult.0 {
+                exit(1)
+            }
+        }
+        
+        if versionResult.0 {
+            Task {
+                _ = await Alert.shared.versionDialog(header: "Running \(AppInfo.displayname): v\(AppInfo.version)", version: versionResult.1, message: "A new version (\(versionResult.1)) is available.", updateAvail: versionResult.0)
+                if prohibited {
+                    exit(1)
+                }
+            }
+        }
+    }
+    
+    func prohibited() async -> [String] {
+        
+        URLCache.shared.removeAllCachedResponses()
+        
+        let versionUrl = URL(string: "https://raw.githubusercontent.com/Jamf-Concepts/api-roles-and-clients/refs/heads/main/resources/version/version-info.json")
+        
+        let configuration = URLSessionConfiguration.ephemeral
+        var request = URLRequest(url: versionUrl!)
+        request.httpMethod = "GET"
+        
+        configuration.httpAdditionalHeaders = ["Accept" : "application/json"]
+        let session = Foundation.URLSession(configuration: configuration, delegate: self as URLSessionDelegate, delegateQueue: OperationQueue.main)
+        do {
+            
+            let checkResult = try await session.data(for: request)
+//            if let responseString = String(data: checkResult.0, encoding: .utf8) {
+//                print("prohibitedVersions: \(responseString)")
+//            } else {
+//                print("prohibitedVersions: No response data returned")
+//            }
+            
+            session.finishTasksAndInvalidate()
+            if let httpResponse = checkResult.1 as? HTTPURLResponse {
+                if httpResponse.statusCode >= 200 && httpResponse.statusCode <= 299 {
+                    let json = try? JSONSerialization.jsonObject(with: checkResult.0, options: .allowFragments)
+                    if let endpointJSON = json as? [String: Any] {
+                        
+                        let prohibitedVersion = (endpointJSON["do_not_run_versions"] as? [String]) ?? []
+                        if prohibitedVersion.count > 0 {
+                            WriteToLog.shared.message("[prohibited] prohibited versions found: \(prohibitedVersion.description)")
+                        }
+                        UserDefaults.standard.set(prohibitedVersion, forKey: "prohibitedVersions")
+                        return prohibitedVersion
+                        
+                    } else {    // if let endpointJSON error
+                        return []
+                    }
+                } else {    // if httpResponse.statusCode <200 or >299
+                    WriteToLog.shared.message("[launchCheck] prohibited response error: \(httpResponse.statusCode)")
+                    return []
+                }
+                
+            } else {
+                return []
+            }
+        } catch {
+            return []
+        }
+    }
+    
+    func version(forceCheck: Bool = false) async -> (Bool, String) {
+        
+        if !forceCheck {
+            WriteToLog.shared.message("[version] skipping version check")
+//            UserDefaults.standard.set(false, forKey: "prohibited") // currently does nothing
+            return (false, AppInfo.version)
+        }
+        
+        URLCache.shared.removeAllCachedResponses()
+
+        let (currMajor, currMinor, currPatch, runningBeta, currBeta) = versionDetails(theVersion: AppInfo.version)
+        
+        var updateAvailable = false
+        var versionTest     = true
+
+        let versionUrl = URL(string: "https://api.github.com/repos/Jamf-Concepts/api-roles-and-clients/releases/latest")
+
+        let configuration = URLSessionConfiguration.ephemeral
+        var request = URLRequest(url: versionUrl!)
+        request.httpMethod = "GET"
+        
+        configuration.httpAdditionalHeaders = ["Accept" : "application/json"]
+        let session = Foundation.URLSession(configuration: configuration, delegate: self as URLSessionDelegate, delegateQueue: OperationQueue.main)
+        do {
+            
+            let checkResult = try await session.data(for: request)
+            
+            session.finishTasksAndInvalidate()
+            if let httpResponse = checkResult.1 as? HTTPURLResponse {
+                if httpResponse.statusCode >= 200 && httpResponse.statusCode <= 299 {
+                    let json = try? JSONSerialization.jsonObject(with: checkResult.0, options: .allowFragments)
+                    if let endpointJSON = json as? [String: Any] {
+                        
+                        let fullVersion = (endpointJSON["tag_name"] as! String).replacingOccurrences(of: "v", with: "")
+                        
+                        if !runningBeta && fullVersion.firstIndex(of: "b") == nil {
+                            versionTest = self.compareVersions(currMajor: currMajor,
+                                                               currMinor: currMinor,
+                                                               currPatch: currPatch,
+                                                               runningBeta: runningBeta,
+                                                               currBeta: currBeta,
+                                                               available: fullVersion)
+                        } else if runningBeta && fullVersion.firstIndex(of: "b") != nil {
+                            versionTest = self.compareVersions(currMajor: currMajor,
+                                                               currMinor: currMinor,
+                                                               currPatch: currPatch,
+                                                               runningBeta: runningBeta,
+                                                               currBeta: currBeta,
+                                                               available: fullVersion)
+                        }
+                        if !versionTest {
+                            updateAvailable = true
+                        }
+
+//                        return (true, "1.2.1")  // for testing
+                        return (updateAvailable, "\(fullVersion)")
+                        
+                    } else {    // if let endpointJSON error
+                        return (false, "")
+                    }
+                } else {    // if httpResponse.statusCode <200 or >299
+                    WriteToLog.shared.message("[version] version check response error: \(httpResponse.statusCode)")
+                    return (false, "")
+                }
+                
+            } else {
+                return (false, "")
+            }
+        } catch {
+            return (false, "")
+        }
+    }
+    
+    private func compareVersions(currMajor: Int, currMinor: Int, currPatch: Int, runningBeta: Bool, currBeta: Int, available: String) -> Bool {
+        var runningCurrent = true
+        var betaVer = ""
+        if runningBeta {
+            betaVer = "b\(currBeta)"
+        }
+        if available != "\(currMajor).\(currMinor).\(currPatch)\(betaVer)" {
+            let (availMajor, availMinor, availPatch, availBeta, availBetaVer) = versionDetails(theVersion: available)
+            if availMajor > currMajor {
+                runningCurrent = false
+            } else if availMajor == currMajor {
+                if availMinor > currMinor {
+                    runningCurrent = false
+                } else if availMinor == currMinor {
+                    if availPatch > currPatch {
+                        runningCurrent = false
+                    } else if availPatch == currPatch && ((runningBeta && availBeta) || (runningBeta && !availBeta))  {
+                        if availBetaVer > currBeta {
+                            runningCurrent = false
+                        }
+                    }
+                }
+            }
+        }
+        return runningCurrent
+    }
+    
+    func versionDetails(theVersion: String) -> (Int, Int, Int, Bool, Int) {
+        var major   = 0
+        var minor   = 0
+        var patch   = 0
+        var betaVer = 0
+        var isBeta  = false
+        
+        let versionArray = theVersion.split(separator: ".")
+        if versionArray.count > 2 {
+
+            major = Int(versionArray[0])!
+            minor = Int(versionArray[1])!
+            let patchArray = versionArray[2].lowercased().split(separator: "b")
+            patch = Int(patchArray[0])!
+            if patchArray.count > 1 {
+                isBeta = true
+                betaVer = Int(patchArray[1])!
+            }
+        }
+        return (major, minor, patch, isBeta, betaVer)
+    }
+    
+    func urlSession(_ session: URLSession, didReceive challenge: URLAuthenticationChallenge, completionHandler: @escaping(  URLSession.AuthChallengeDisposition, URLCredential?) -> Void) {
+        completionHandler(.useCredential, URLCredential(trust: challenge.protectionSpace.serverTrust!))
+    }
+}
+
--- ObjectInfo/WriteToLog.swift
+++ ObjectInfo/WriteToLog.swift
@@ -1,150 +1,79 @@
 //
-//  WriteToLog.swift
-//  Object Info
-//
-//  Created by Leslie Helou on 1/8/21.
-//  Copyright © 2021 jamf. All rights reserved.
+//  Copyright 2025 Jamf. All rights reserved.
 //
 
 import Foundation
+
+struct Log {
+    static var path          = "" //(NSHomeDirectory() + "/Library/Logs/")
+    static var file          = "apiRolesClients.log"
+    static var filePath      = ""
+    static var maxFiles      = 42
+}
+
 class WriteToLog {
     
     static let shared = WriteToLog()
-    private init() { }
     
     let fm = FileManager()
     
-    func createLogFile(completionHandler: @escaping (_ result: String) -> Void) {
-        if !self.fm.fileExists(atPath: Log.path! + Log.file) {
-            self.fm.createFile(atPath: Log.path! + Log.file, contents: nil, attributes: nil)
-        }
-        completionHandler("created")
-    }
-    
-    // func logCleanup - start
     func logCleanup() {
+        
+        if didRun {
             var logArray: [String] = []
             var logCount: Int = 0
             do {
-                let logFiles = try fm.contentsOfDirectory(atPath: Log.path!)
+                let logFiles = try fm.contentsOfDirectory(atPath: Log.path)
                 
                 for logFile in logFiles {
-                    if logFile.contains(".zip") {
-                        let filePath: String = Log.path! + logFile
-                        logArray.append(filePath)
-                    }
+                    let filePath: String = Log.path + "/" + logFile
+                    logArray.append(filePath)
                 }
                 logArray.sort()
                 logCount = logArray.count
                 // remove old log files
-                if logCount > Log.maxFiles {
+                if logCount-1 >= Log.maxFiles {
                     for i in (0..<logCount-Log.maxFiles) {
-                        WriteToLog.shared.message(stringOfText: "Deleting log file: " + logArray[i] + "\n")
-                        
+                        message("Deleting log file: " + logArray[i] + "\n")
                         do {
                             try fm.removeItem(atPath: logArray[i])
                         }
                         catch let error as NSError {
-                            WriteToLog.shared.message(stringOfText: "Error deleting log file:\n    " + logArray[i] + "\n    \(error)\n")
-                        }
-                    }
-                }
-                // zip current log if it's over 5MB
-                do {
-                    let fileAttributes = try fm.attributesOfItem(atPath: Log.path! + Log.file)
-                    let logSize = fileAttributes[.size] as? Int
-                    if Int("\(logSize ?? 0)")! > Log.maxSize {
-                        let dateTmpArray = getCurrentTime().split(separator: "_")
-                        let dateStamp    = dateTmpArray[0]
-                        zipIt(args: "/usr/bin/zip -rm -jj -o \(Log.path!)ObjectInfo_\(dateStamp) \(Log.path!)\(Log.file)") {
-                            (result: String) in
-//                            print("zipIt result: \(result)")
-                            self.createLogFile() {
-                                (result: String) in
-
-                                return
-                            }
+                            message("Error deleting log file:\n    " + logArray[i] + "\n    \(error)\n")
                         }
                     }
                 }
             } catch {
-//                print("no history")
-                return
+                print("no history")
             }
-    }
-    // func logCleanup - end
-
-    
-    var logFileW: FileHandle? = FileHandle(forUpdatingAtPath: "")
-
-    func message(stringOfText: String) {
-        let logString = "\(getCurrentTime()) \(stringOfText)\n"
-
-        self.logFileW = FileHandle(forUpdatingAtPath: (Log.path! + Log.file))
-        let fullpath = Log.path! + Log.file
-        
-        let historyText = (logString as NSString).data(using: String.Encoding.utf8.rawValue)
-        self.logFileW?.seekToEndOfFile()
-        self.logFileW?.write(historyText!)
-    }
-    
-    func getCurrentTime() -> String {
-        let current = Date()
-        let localCalendar = Calendar.current
-        let dateObjects: Set<Calendar.Component> = [.year, .month, .day, .hour, .minute, .second]
-        let dateTime      = localCalendar.dateComponents(dateObjects, from: current)
-        let currentMonth  = leadingZero(value: dateTime.month!)
-        let currentDay    = leadingZero(value: dateTime.day!)
-        let currentHour   = leadingZero(value: dateTime.hour!)
-        let currentMinute = leadingZero(value: dateTime.minute!)
-        let currentSecond = leadingZero(value: dateTime.second!)
-        let stringDate    = "\(dateTime.year!)\(currentMonth)\(currentDay)_\(currentHour)\(currentMinute)\(currentSecond)"
-        return stringDate
-    }
-    
-    func logDate() -> String {
-        let logDate = DateFormatter()
-        logDate.dateFormat = "E MMM dd HH:mm:ss"
-        return("\(logDate.string(from: Date()))")
-    }
-    
-    // add leading zero to single digit integers
-    func leadingZero(value: Int) -> String {
-        var formattedValue = ""
-        if value < 10 {
-            formattedValue = "0\(value)"
         } else {
-            formattedValue = "\(value)"
+            // delete empty log file
+            do {
+                try fm.removeItem(atPath: Log.filePath)
+            }
+            catch let error as NSError {
+                message("Error deleting log file:    \n" + Log.filePath + "\n    \(error)\n")
+            }
         }
-        return formattedValue
     }
-    
-    func zipIt(args: String..., completion: @escaping (_ result: String) -> Void) {
 
-        var cmdArgs = ["-c"]
-        for theArg in args {
-            cmdArgs.append(theArg)
+    func message(_ message: String) {
+        let logString = "\(getCurrentTime()) \(message)\n"
+        NSLog(logString)
+
+        guard let logData = logString.data(using: .utf8) else { return }
+        let logURL = URL(fileURLWithPath: Log.filePath)
+
+        do {
+            let fileHandle = try FileHandle(forWritingTo: logURL)
+            defer { fileHandle.closeFile() } // Ensure file is closed
+            
+            fileHandle.seekToEndOfFile()
+            fileHandle.write(logData)
+        } catch {
+//            print("[Log Error] Failed to write to log file: \(error.localizedDescription)")
+            NSLog("[Log Error] Failed to write to log file: \(error.localizedDescription)")
         }
-        var status  = ""
-        var statusArray  = [String]()
-        let pipe    = Pipe()
-        let task    = Process()
-        
-        task.launchPath     = "/bin/sh"
-        task.arguments      = cmdArgs
-        task.standardOutput = pipe
-        
-        task.launch()
-        
-        let outdata = pipe.fileHandleForReading.readDataToEndOfFile()
-        if var string = String(data: outdata, encoding: .utf8) {
-            string = string.trimmingCharacters(in: .newlines)
-            statusArray = string.components(separatedBy: "\n")
-            status = statusArray[0]
-        }
-        
-        task.waitUntilExit()
-        completion(status)
     }
 
 }
